d6856555d3e184d05abcf71c3bab183db7d89f23
literature <65801567+LiteraturePro@users.noreply.github.com>
2021-02-24 13:36:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ from PIL import Image
 from io import BytesIO
 from flask import Flask, render_template, make_response, flash
 import flask
-from flask_ngrok import run_with_ngrok #to run the application on colab using ngrok
 
 import inference_onnx
 
 app = Flask(__name__)
 
-run_with_ngrok(app)   #starts ngrok when the app is run
+#run_with_ngrok(app)   #starts ngrok when the app is run
 
 def convert_bytes_to_image(img_name,img_bytes):
     #将bytes结果转化为字节流
@@ -67,4 +66,4 @@ def api():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
